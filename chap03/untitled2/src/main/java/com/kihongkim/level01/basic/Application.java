b5fd8e28d84d12aea7e7b1ac607ea8f20e075882
kihongkim <clevermind2@naver.com>
2025-02-18 21:37:36 +0900

    no message

--- chap03/untitled2/src/main/java/com/kihongkim/level01/basic/Application.java
+++
@@ -1,8 +0,0 @@
-package com.kihongkim.level01.basic;
-
-public class Application {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.checkMethod();
-    }
-}
--- chap03/untitled2/src/main/java/com/kihongkim/level01/basic/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.kihongkim.level01.basic;
-
-public class Calculator {
-    public static void main(String[] args) {
-
-    }
-    public void checkMethod() {
-        System.out.println("메서드 호출 확인");
-}
